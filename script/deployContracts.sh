df931cff54b12fea6c530c7110195e5408727ef5
Matjaz Verbole <verbole@gmail.com>
2023-11-03 11:11:21 +0100

    Update deployContracts script to support L1 and L2 RPC providers

--- script/deployContracts.sh
+++ script/deployContracts.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 cd ../
-forge script --rpc-url=http://localhost:8545 --broadcast --verify -vvvv script/L1LiskToken.s.sol:L1LiskTokenScript
-forge script --rpc-url=http://localhost:8545 --broadcast --verify -vvvv script/L2LiskToken.s.sol:L2LiskTokenScript
-forge script --rpc-url=http://localhost:8545 --broadcast --verify -vvvv script/L2Claim.s.sol:L2ClaimScript
-forge script --rpc-url=http://localhost:8545 --broadcast --verify -vvvv script/TransferFunds.s.sol:TransferFundsScript
+source .env
+forge script --rpc-url="$L1_RPC_URL" --broadcast --verify -vvvv script/L1LiskToken.s.sol:L1LiskTokenScript
+forge script --rpc-url="$L2_RPC_URL" --broadcast --verify -vvvv script/L2LiskToken.s.sol:L2LiskTokenScript
+forge script --rpc-url="$L2_RPC_URL" --broadcast --verify -vvvv script/L2Claim.s.sol:L2ClaimScript
+forge script --rpc-url="$L1_RPC_URL" --broadcast --verify -vvvv script/TransferFunds.s.sol:TransferFundsScript
