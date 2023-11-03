f60b6a8f08aa0ba389ac2d0d637ec467114db135
Matjaz Verbole <verbole@gmail.com>
2023-11-03 12:25:58 +0100

    Remove duplicated import

--- src/L2/L2LiskToken.sol
+++ src/L2/L2LiskToken.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.21;
 
-import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
