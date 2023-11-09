fefaad3b6163f777bb862decf381dd5f2084984d
Matjaz Verbole <verbole@gmail.com>
2023-11-09 08:35:34 +0100

    Deployed contracts can be verified on Etherscan

--- script/deployContracts.sh
+++ script/deployContracts.sh
@@ -2,7 +2,7 @@
 
 cd ../
 source .env
-forge script --rpc-url="$L1_RPC_URL" --broadcast --verify -vvvv script/L1LiskToken.s.sol:L1LiskTokenScript
-forge script --rpc-url="$L2_RPC_URL" --broadcast --verify -vvvv script/L2LiskToken.s.sol:L2LiskTokenScript
-forge script --rpc-url="$L2_RPC_URL" --broadcast --verify -vvvv script/L2Claim.s.sol:L2ClaimScript
-forge script --rpc-url="$L1_RPC_URL" --broadcast --verify -vvvv script/TransferFunds.s.sol:TransferFundsScript
+forge script --rpc-url="$L1_RPC_URL" --broadcast --verify --etherscan-api-key="$L1_ETHERSCAN_API_KEY" -vvvv script/L1LiskToken.s.sol:L1LiskTokenScript
+forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --etherscan-api-key="$L2_ETHERSCAN_API_KEY" -vvvv script/L2LiskToken.s.sol:L2LiskTokenScript
+forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --etherscan-api-key="$L2_ETHERSCAN_API_KEY" -vvvv script/L2Claim.s.sol:L2ClaimScript
+forge script --rpc-url="$L1_RPC_URL" --broadcast -vvvv script/TransferFunds.s.sol:TransferFundsScript
