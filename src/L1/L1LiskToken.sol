5c30171d4c901f24320b3a3aafd30af9d0c2a776
has5aan <has5aan@outlook.com>
2023-12-10 02:49:31 +0100

    L1LiskToken maintains burners through BurnerRole contract

--- src/L1/L1LiskToken.sol
+++ src/L1/L1LiskToken.sol
@@ -3,13 +3,22 @@ pragma solidity 0.8.21;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "../utils/ownership/Ownable.sol";
+import { BurnerRole } from "../access/roles/BurnerRole.sol";
 
-contract L1LiskToken is ERC20, Ownable {
+contract L1LiskToken is ERC20, Ownable, BurnerRole {
     string private constant NAME = "Lisk";
     string private constant SYMBOL = "LSK";
     uint256 private constant TOTAL_SUPPLY = 200_000_000 * 10 ** 18; //200 million LSK tokens
 
     constructor() ERC20(NAME, SYMBOL) {
         _mint(_msgSender(), TOTAL_SUPPLY);
+    }
+
+    function addBurner(address account) public override onlyOwner {
+        _addBurner(account);
+    }
+
+    function renounceBurner(address account) public override onlyOwner {
+        _removeBurner(account);
     }
 }
--- test/L1/L1LiskToken.t.sol
+++ test/L1/L1LiskToken.t.sol
@@ -3,8 +3,12 @@ pragma solidity 0.8.21;
 
 import { Test, console2 } from "forge-std/Test.sol";
 import { L1LiskToken } from "src/L1/L1LiskToken.sol";
+import { Unauthorized } from "src/utils/Errors.sol";
 
 contract L1LiskTokenTest is Test {
+    event BurnerAdded(address indexed account);
+    event BurnerRemoved(address indexed account);
+
     string private constant NAME = "Lisk";
     string private constant SYMBOL = "LSK";
     uint256 private constant TOTAL_SUPPLY = 200_000_000 * 10 ** 18; //200 million LSK tokens
@@ -21,5 +25,39 @@ contract L1LiskTokenTest is Test {
         assertEq(l1LiskToken.totalSupply(), TOTAL_SUPPLY);
         assertEq(l1LiskToken.balanceOf(address(this)), TOTAL_SUPPLY);
         assertEq(l1LiskToken.owner(), address(this));
+    }
+
+    function test_addAndRenounceBurner() public {
+        address alice = address(0x1);
+
+        vm.expectEmit(true, false, false, true, address(l1LiskToken));
+        emit BurnerAdded(alice);
+        l1LiskToken.addBurner(alice);
+        assertTrue(l1LiskToken.isBurner(alice));
+
+        vm.expectEmit(true, false, false, true, address(l1LiskToken));
+        emit BurnerRemoved(alice);
+        l1LiskToken.renounceBurner(alice);
+        assertFalse(l1LiskToken.isBurner(alice));
+    }
+
+    function test_onlyOwnerAddsOrRenouncesBurner() public {
+        address alice = address(0x1);
+
+        vm.startPrank(alice);
+
+        vm.expectRevert(Unauthorized.selector);
+        l1LiskToken.addBurner(alice);
+
+        vm.expectRevert(Unauthorized.selector);
+        l1LiskToken.renounceBurner(alice);
+
+        vm.stopPrank();
+
+        l1LiskToken.addBurner(alice);
+        assertTrue(l1LiskToken.isBurner(alice));
+
+        l1LiskToken.renounceBurner(alice);
+        assertFalse(l1LiskToken.isBurner(alice));
     }
 }
