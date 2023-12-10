60621e0b079d1a91f78a11f1c17c3275761e8f76
has5aan <has5aan@outlook.com>
2023-12-10 04:08:33 +0100

    L1LiskToken is ERC20Permit contract

--- src/L1/L1LiskToken.sol
+++ src/L1/L1LiskToken.sol
@@ -2,15 +2,16 @@
 pragma solidity 0.8.21;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import { Ownable } from "../utils/ownership/Ownable.sol";
 import { BurnerRole } from "../access/roles/BurnerRole.sol";
 
-contract L1LiskToken is ERC20, Ownable, BurnerRole {
+contract L1LiskToken is ERC20, ERC20Permit, Ownable, BurnerRole {
     string private constant NAME = "Lisk";
     string private constant SYMBOL = "LSK";
     uint256 private constant TOTAL_SUPPLY = 200_000_000 * 10 ** 18; //200 million LSK tokens
 
-    constructor() ERC20(NAME, SYMBOL) {
+    constructor() ERC20(NAME, SYMBOL) ERC20Permit(NAME) {
         _mint(_msgSender(), TOTAL_SUPPLY);
     }
 
