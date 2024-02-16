b48e3e75115245838a171c83ec74c8680bd7f5f7
Matjaz Verbole <verbole@gmail.com>
2024-02-16 08:44:16 +0100

    Governor contract implementation

---
+++ test/L2/L2Governor.t.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.23;
+
+import { IVotes } from "@openzeppelin-upgradeable/contracts/governance/extensions/GovernorVotesUpgradeable.sol";
+import { OwnableUpgradeable } from "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import { TimelockController } from "@openzeppelin/contracts/governance/TimelockController.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { Test, console } from "forge-std/Test.sol";
+import { L2Governor } from "src/L2/L2Governor.sol";
+import { Utils } from "script/Utils.sol";
+
+contract L2GovernorV2 is L2Governor {
+    uint256 public testNumber;
+
+    function initializeV2(uint256 _testNumber) public reinitializer(2) {
+        testNumber = _testNumber;
+    }
+
+    function version() public pure virtual override returns (string memory) {
+        return "2.0.0";
+    }
+
+    function onlyV2() public pure returns (string memory) {
+        return "Only L2GovernorV2 have this function";
+    }
+}
+
+contract L2GovernorTest is Test {
+    Utils public utils;
+    L2Governor public l2GovernorImplementation;
+    L2Governor public l2Governor;
+
+    IVotes votingPower;
+    TimelockController timelock;
+    address initialOwner;
+
+    function setUp() public {
+        utils = new Utils();
+
+        // set initial values
+        votingPower = IVotes(address(0x1));
+        timelock = new TimelockController(0, new address[](0), new address[](0), address(this));
+        initialOwner = address(this);
+
+        console.log("L2GovernorTest address is: %s", address(this));
+
+        // deploy L2Governor Implementation contract
+        l2GovernorImplementation = new L2Governor();
+
+        // deploy L2Governor contract via proxy and initialize it at the same time
+        l2Governor = L2Governor(
+            payable(
+                address(
+                    new ERC1967Proxy(
+                        address(l2GovernorImplementation),
+                        abi.encodeWithSelector(l2Governor.initialize.selector, votingPower, timelock, initialOwner)
+                    )
+                )
+            )
+        );
+
+        assertEq(l2Governor.name(), "Lisk Governor");
+        assertEq(l2Governor.version(), "1.0.0");
+        assertEq(l2Governor.votingDelay(), 0);
+        assertEq(l2Governor.votingPeriod(), 50400);
+        assertEq(l2Governor.proposalThreshold(), 2e18);
+        assertEq(l2Governor.timelock(), address(timelock));
+        assertEq(address(l2Governor.token()), address(votingPower));
+        assertEq(l2Governor.owner(), initialOwner);
+    }
+
+    function test_UpgradeToAndCall_RevertWhenNotOwner() public {
+        // deploy L2GovernorV2 implementation contract
+        L2GovernorV2 l2GovernorV2Implementation = new L2GovernorV2();
+        address nobody = vm.addr(1);
+
+        vm.prank(nobody);
+        vm.expectRevert(abi.encodeWithSelector(OwnableUpgradeable.OwnableUnauthorizedAccount.selector, nobody));
+        l2Governor.upgradeToAndCall(address(l2GovernorV2Implementation), "");
+    }
+
+    function test_UpgradeToAndCall_SuccessUpgrade() public {
+        // deploy L2GovernorV2 implementation contract
+        L2GovernorV2 l2GovernorV2Implementation = new L2GovernorV2();
+
+        uint256 testNumber = 123;
+
+        // upgrade contract, and also change some variables by reinitialize
+        l2Governor.upgradeToAndCall(
+            address(l2GovernorV2Implementation),
+            abi.encodeWithSelector(l2GovernorV2Implementation.initializeV2.selector, testNumber)
+        );
+
+        // wrap L2Governor proxy with new contract
+        L2GovernorV2 l2GovernorV2 = L2GovernorV2(payable(address(l2Governor)));
+
+        // assure version is updated
+        assertEq(l2GovernorV2.version(), "2.0.0");
+
+        // new testNumber variable introduced
+        assertEq(l2GovernorV2.testNumber(), testNumber);
+
+        // new function introduced
+        assertEq(l2GovernorV2.onlyV2(), "Only L2GovernorV2 have this function");
+
+        // assure cannot re-reinitialize
+        vm.expectRevert();
+        l2GovernorV2.initializeV2(testNumber + 1);
+    }
+}
