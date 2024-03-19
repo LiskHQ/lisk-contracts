eb3aad8f92c44f74c03ae49da10d60a1f7da5889
Alessandro Ricottone <ricott2@gmail.com>
2024-03-19 13:19:03 +0100

    Add more balance for tests

--- script/runL1TestNetwork.sh
+++ script/runL1TestNetwork.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source ../.env
-anvil --mnemonic "$TEST_NETWORK_MNEMONIC" --port 8545 --fork-url "$L1_FORK_RPC_URL"
+anvil --mnemonic "$TEST_NETWORK_MNEMONIC" --port 8545 --fork-url "$L1_FORK_RPC_URL" --balance 200000
