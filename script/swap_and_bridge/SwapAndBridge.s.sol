b5af0f962de9e0b83c8cc53d05da2903464e989b
Alessandro Ricottone <ricott2@gmail.com>
2024-03-19 12:05:08 +0100

    Clean up deployment script

--- script/swap_and_bridge/SwapAndBridge.s.sol
+++ script/swap_and_bridge/SwapAndBridge.s.sol
@@ -3,21 +3,13 @@ pragma solidity 0.8.23;
 
 import { Script, console2 } from "forge-std/Script.sol";
 import { SwapAndBridge } from "src/L1/SwapAndBridge.sol";
-import "script/Utils.sol";
 
 /// @title SwapAndBridgeScript - SwapAndBridge deployment script
 /// @notice This contract is used to deploy SwapAndBridge contract.
 contract SwapAndBridgeScript is Script {
-    /// @notice Utils contract which provides functions to read and write JSON files containing L1 and L2 addresses.
-    Utils utils;
-
-    function setUp() public {
-        utils = new Utils();
-    }
-
     /// @notice This function deploys the SwapAndBridge contract.
     function run() public {
-        // Deployer's private key. Owner of the L1 Lisk token. PRIVATE_KEY is set in .env file.
+        // Deployer's private key. PRIVATE_KEY is set in .env file.
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         console2.log("Deploying SwapAndBirdge contract for Lido...");
@@ -33,18 +25,5 @@ contract SwapAndBridgeScript is Script {
         assert(address(swapAndBridgeLido) != address(0));
         console2.log("SwapAndBridge (Lido) successfully deployed!");
         console2.log("SwapAndBridge (Lido) address: %s", address(swapAndBridgeLido));
-        utils.writeL1AddressToFile(".L1LidoSwap", address(swapAndBridgeLido));
-
-        // deploy SwapAndBirdge contract for Diva
-        // vm.startBroadcast(deployerPrivateKey);
-        // SwapAndBridge swapAndBridgeDiva = new SwapAndBridge(
-        //     vm.envAddress("L1_STANDARD_BRIDGE_ADDR"),
-        //     vm.envAddress("L1_DIVA_TOKEN_ADDR"),
-        //     vm.envAddress("L2_DIVA_TOKEN_ADDR")
-        // );
-        // vm.stopBroadcast();
-        // assert(address(swapAndBridgeDiva) != address(0));
-        // console2.log("SwapAndBridge (Diva) successfully deployed!");
-        // console2.log("SwapAndBridge (Diva) address: %s", address(swapAndBridgeDiva));
     }
 }
