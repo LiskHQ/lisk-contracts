2be03cd8474cfd606fcacf265c429e95f46c93cc
Matjaz Verbole <verbole@gmail.com>
2024-04-16 13:02:39 +0200

    Rename all interface files to start with capital letter

--- script/generateInterfaces.sh
+++ script/generateInterfaces.sh
@@ -8,29 +8,29 @@ INTERFACE_L1_DIR="../src/interfaces/L1"
 INTERFACE_L2_DIR="../src/interfaces/L2"
 
 echo "Generating interface for L1LiskToken smart contract..."
-cast interface -o $INTERFACE_L1_DIR/iL1LiskToken.sol -n IL1LiskToken .././out/L1LiskToken.sol/L1LiskToken.json
+cast interface -o $INTERFACE_L1_DIR/IL1LiskToken.sol -n IL1LiskToken .././out/L1LiskToken.sol/L1LiskToken.json
 echo "Done."
 
 echo "Generating interface for L2LiskToken smart contract..."
-cast interface -o $INTERFACE_L2_DIR/iL2LiskToken.sol -n IL2LiskToken .././out/L2LiskToken.sol/L2LiskToken.json
+cast interface -o $INTERFACE_L2_DIR/IL2LiskToken.sol -n IL2LiskToken .././out/L2LiskToken.sol/L2LiskToken.json
 echo "Done."
 
 echo "Generating interface for L2Claim smart contract..."
-cast interface -o $INTERFACE_L2_DIR/iL2Claim.sol -n IL2Claim .././out/L2Claim.sol/L2Claim.json
+cast interface -o $INTERFACE_L2_DIR/IL2Claim.sol -n IL2Claim .././out/L2Claim.sol/L2Claim.json
 echo "Done."
 
 echo "Generating interface for L2VotingPower smart contract..."
-cast interface -o $INTERFACE_L2_DIR/iL2VotingPower.sol -n IL2VotingPower .././out/L2VotingPower.sol/L2VotingPower.json
+cast interface -o $INTERFACE_L2_DIR/IL2VotingPower.sol -n IL2VotingPower .././out/L2VotingPower.sol/L2VotingPower.json
 echo "Done."
 
 echo "Generating interface for L2Governor smart contract..."
-cast interface -o $INTERFACE_L2_DIR/iL2Governor.sol -n IL2Governor .././out/L2Governor.sol/L2Governor.json
+cast interface -o $INTERFACE_L2_DIR/IL2Governor.sol -n IL2Governor .././out/L2Governor.sol/L2Governor.json
 echo "Done."
 
 echo "Generating interface for L2LockingPosition smart contract..."
-cast interface -o $INTERFACE_L2_DIR/iL2LockingPosition.sol -n IL2LockingPosition .././out/L2LockingPosition.sol/L2LockingPosition.json
+cast interface -o $INTERFACE_L2_DIR/IL2LockingPosition.sol -n IL2LockingPosition .././out/L2LockingPosition.sol/L2LockingPosition.json
 echo "Done."
 
 echo "Generating interface for L2Staking smart contract..."
-cast interface -o $INTERFACE_L2_DIR/iL2Staking.sol -n IL2Staking .././out/L2Staking.sol/L2Staking.json
+cast interface -o $INTERFACE_L2_DIR/IL2Staking.sol -n IL2Staking .././out/L2Staking.sol/L2Staking.json
 echo "Done."
