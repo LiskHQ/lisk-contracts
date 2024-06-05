43ea90094e9f98df06885c4e8fdfebdb61063b86
Incede <33103370+Incede@users.noreply.github.com>
2024-06-05 17:18:59 +0200

    Update to use factory proxy instead of implementation

--- script/6_deployUSDTContract.sh
+++ script/6_deployUSDTContract.sh
@@ -10,4 +10,4 @@ echo "Done."
 
 echo "Deploying OptimismMintableERC20 USDT token smart contract..."
 # Use flag --nonce to specify suitable nonce in case of nonce mismatch
-cast send --rpc-url $L2_RPC_URL "0xc0D3c0d3C0d3c0d3c0D3c0d3c0D3c0D3c0D30012" "createOptimismMintableERC20WithDecimals(address,string,string,uint8)" $REMOTE_TOKEN_ADDR_USDT "Tether USD" "USDT" 6 -i
+cast send --rpc-url $L2_RPC_URL "0x4200000000000000000000000000000000000012" "createOptimismMintableERC20WithDecimals(address,string,string,uint8)" $REMOTE_TOKEN_ADDR_USDT "Tether USD" "USDT" 6 -i
