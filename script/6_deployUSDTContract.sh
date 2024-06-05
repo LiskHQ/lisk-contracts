62bab0ec6f2b3548390441a1f7bea9e8ef61dbb7
Incede <33103370+Incede@users.noreply.github.com>
2024-06-05 16:34:57 +0200

    Further feedback

--- script/6_deployUSDTContract.sh
+++ script/6_deployUSDTContract.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-source ../.env.testnet
+echo "Setting environment variables..."
+source .env
+echo "Done."
+
 echo "Deploying OptimismMintableERC20 USDT token smart contract..."
 # Use flag --nonce to specify suitable nonce in case of nonce mismatch
 cast send --rpc-url $L2_RPC_URL "0xc0D3c0d3C0d3c0d3c0D3c0d3c0D3c0D3c0D30012" "createOptimismMintableERC20WithDecimals(address,string,string,uint8)" $REMOTE_TOKEN_ADDR_USDT "Tether USD" "USDT" 6 -i
