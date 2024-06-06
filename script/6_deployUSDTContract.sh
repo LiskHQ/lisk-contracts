840a41476a620b36d52f9d44f5bd85246d46b342
Incede <33103370+Incede@users.noreply.github.com>
2024-06-06 09:11:02 +0200

    Add script to deploy OptimismMintableERC20 USDT token using OptimismMintableERC20Factory (#155)
    * Add script to deploy standard OP token using factory
    
    * Change decimals to 6
    
    * Hardcode factory address
    
    * Update file name
    
    * Hardcode params to USDT
    
    * Update per feedback
    
    * Further feedback
    
    * Update source to cal from root dir
    
    * Traverse to root and call source as per feedback
    
    * Update to use factory proxy instead of implementation
    
    * Add additional check for non zero exit status per feedback

---
+++ script/6_deployUSDTContract.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Instructing the shell to exit immediately if any command returns a non-zero exit status..."
+set -e
+echo "Done."
+
+echo "Navigating to the root directory of the project..."
+cd ../
+echo "Done."
+
+echo "Setting environment variables..."
+source .env
+echo "Done."
+
+echo "Deploying OptimismMintableERC20 USDT token smart contract..."
+# Use flag --nonce to specify suitable nonce in case of nonce mismatch
+cast send --rpc-url $L2_RPC_URL "0x4200000000000000000000000000000000000012" "createOptimismMintableERC20WithDecimals(address,string,string,uint8)" $REMOTE_TOKEN_ADDR_USDT "Tether USD" "USDT" 6 -i
