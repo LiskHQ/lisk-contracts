e3e23211b8116dc5abfdbf27e298548c14945a82
Alessandro Ricottone <ricott2@gmail.com>
2024-06-10 16:08:32 +0200

    remove unnecessary contracts

--- script/swap_and_bridge/contracts/SwapAndBridge.s.sol
+++ script/swap_and_bridge/contracts/SwapAndBridge.s.sol
@@ -3,74 +3,20 @@ pragma solidity 0.8.23;
 
 import { Script, console2 } from "forge-std/Script.sol";
 import { SwapAndBridge } from "src/L1/SwapAndBridge.sol";
-import { OptimismMintableERC20 } from "@optimism/universal/OptimismMintableERC20.sol";
-import "script/contracts/Utils.sol";
 
 /// @title SwapAndBridgeScript - SwapAndBridge deployment script
 /// @notice This contract is used to deploy SwapAndBridge contract.
-contract SwapAndBridgeDivaScript is Script {
-    /// @notice Utils contract which provides functions to read and write JSON files containing L1 and L2 addresses.
-    Utils utils;
-
-    function setUp() public {
-        utils = new Utils();
-    }
-
+contract SwapAndBridgeScript is Script {
     /// @notice This function deploys the SwapAndBridge contract.
-    function run() public {
+    function run(address bridgeAddress, address l1Token, address l2Token) public {
         // Deployer's private key. L1_SWAP_AND_BRIDGE_DEPLOYER_PRIVATE_KEY is set in .env file.
-        uint256 deployerPrivateKey = vm.envUint("L1_SWAP_AND_BRIDGE_DEPLOYER_PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
-        // read swap and bridge addresses from file
-        Utils.SwapAndBridgeAddressesConfig memory swapAndBridgeAddressesConfig = utils.readSwapAndBridgeAddressesFile();
-
-        console2.log("Deploying SwapAndBridge contract for Diva...");
+        console2.log("Deploying SwapAndBridge contract...");
         vm.startBroadcast(deployerPrivateKey);
-        SwapAndBridge swapAndBridgeDiva = new SwapAndBridge(
-            vm.envAddress("L1_DIVA_BRIDGE_ADDR"),
-            vm.envAddress("L1_DIVA_TOKEN_ADDR"),
-            swapAndBridgeAddressesConfig.L2WdivETH
-        );
+        SwapAndBridge swapAndBridge = new SwapAndBridge(bridgeAddress, l1Token, l2Token);
         vm.stopBroadcast();
-        assert(address(swapAndBridgeDiva) != address(0));
-        console2.log("SwapAndBridge (Diva) successfully deployed at address: %s", address(swapAndBridgeDiva));
-
-        // write to json
-        swapAndBridgeAddressesConfig.SwapAndBridgeDiva = address(swapAndBridgeDiva);
-        utils.writeSwapAndBridgeAddressesFile(swapAndBridgeAddressesConfig);
-    }
-}
-
-/// @title SwapAndBridgeScript - SwapAndBridge deployment script
-/// @notice This contract is used to deploy SwapAndBridge contract.
-contract SwapAndBridgeLidoScript is Script {
-    /// @notice Utils contract which provides functions to read and write JSON files containing L1 and L2 addresses.
-    Utils utils;
-
-    function setUp() public {
-        utils = new Utils();
-    }
-    /// @notice This function deploys the SwapAndBridge contract.
-
-    function run() public {
-        // Deployer's private key. L1_SWAP_AND_BRIDGE_DEPLOYER_PRIVATE_KEY is set in .env file.
-        uint256 deployerPrivateKey = vm.envUint("L1_SWAP_AND_BRIDGE_DEPLOYER_PRIVATE_KEY");
-        // read swap and bridge addresses from file
-        Utils.SwapAndBridgeAddressesConfig memory swapAndBridgeAddressesConfig = utils.readSwapAndBridgeAddressesFile();
-
-        console2.log("Deploying SwapAndBridge contract for Lido...");
-        vm.startBroadcast(deployerPrivateKey);
-        SwapAndBridge swapAndBridgeLido = new SwapAndBridge(
-            vm.envAddress("L1_LIDO_BRIDGE_ADDR"),
-            vm.envAddress("L1_LIDO_TOKEN_ADDR"),
-            vm.envAddress("L2_LIDO_TOKEN_ADDR")
-        );
-        vm.stopBroadcast();
-        assert(address(swapAndBridgeLido) != address(0));
-        console2.log("SwapAndBridge (Lido) successfully deployed at address: %s", address(swapAndBridgeLido));
-
-        // write to json
-        swapAndBridgeAddressesConfig.SwapAndBridgeLido = address(swapAndBridgeLido);
-        utils.writeSwapAndBridgeAddressesFile(swapAndBridgeAddressesConfig);
+        assert(address(swapAndBridge) != address(0));
+        console2.log("SwapAndBridge successfully deployed at address: %s", address(swapAndBridge));
     }
 }
