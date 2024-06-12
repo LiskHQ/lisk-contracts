6fa2f98a2b626eb928fcfd5b8c4152ba8e322562
Franco NG <franco.ng@lightcurve.io>
2024-06-12 14:55:40 +0200

    Added L2VotingPowerPaused and L2GovernorPaused contract with test cases (#165)
    * Added L2VotingPowerPaused and L2GovernorPaused contract with test cases
    
    * Add deployment script
    
    * Update git submodules
    
    * Add foundry.toml
    
    * Change version function to be independent
    
    * Update comments
    
    * Update gitsubmodule, added Custom Errors
    
    * Rearrange functions according to requirements
    
    * pump forge-std to 1.8.2
    
    * Add onReceive ERC721 and ERC1155 check
    
    * Test adjustVotingPower is unpaused
    
    * Add deployPausedDAO.sh
    
    * Add forge clean to deploy script
    
    * Clean before every forge script runs
    
    * Used `../../` to navigate paused script
    
    * rename assertInitParamsEq

---
+++ src/L2/paused/L2VotingPowerPaused.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.23;
+
+import { Ownable2StepUpgradeable } from "@openzeppelin-upgradeable/contracts/access/Ownable2StepUpgradeable.sol";
+import { Initializable } from "@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import { L2VotingPower } from "../L2VotingPower.sol";
+
+contract L2VotingPowerPaused is L2VotingPower {
+    error VotingPowerIsPaused();
+
+    /// @notice Setting global params.
+    function initializePaused() public reinitializer(2) {
+        version = "1.0.0-paused";
+    }
+
+    /// @notice Override the modifyLockingPosition function to pause VotingPower interactions.
+    function delegate(address) public virtual override {
+        revert VotingPowerIsPaused();
+    }
+
+    /// @notice Override the modifyLockingPosition function to pause VotingPower interactions.
+    function delegateBySig(address, uint256, uint256, uint8, bytes32, bytes32) public virtual override {
+        revert VotingPowerIsPaused();
+    }
+}
---
+++ test/L2/paused/L2VotingPowerPaused.t.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.23;
+
+import { IVotes } from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { OwnableUpgradeable } from "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
+import { IERC20Errors } from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Test, console } from "forge-std/Test.sol";
+import { L2VotingPower } from "src/L2/L2VotingPower.sol";
+import { L2VotingPowerPaused } from "src/L2/paused/L2VotingPowerPaused.sol";
+import { IL2LockingPosition } from "src/interfaces/L2/IL2LockingPosition.sol";
+import { Utils } from "script/contracts/Utils.sol";
+
+contract MockL2VotingPowerV2 is L2VotingPower {
+    uint256 public testNumber;
+
+    function initializeV2(uint256 _testNumber) public reinitializer(3) {
+        version = "2.0.0";
+        testNumber = _testNumber;
+    }
+
+    function onlyV2() public pure returns (string memory) {
+        return "Only L2VotingPowerV2 have this function";
+    }
+}
+
+contract L2VotingPowerPausedTest is Test {
+    Utils public utils;
+    L2VotingPower public l2VotingPowerImplementation;
+    L2VotingPower public l2VotingPower;
+
+    address lockingPositionContractAddress;
+
+    function assertInitParamsEq() internal {
+        assertEq(l2VotingPower.lockingPositionAddress(), lockingPositionContractAddress);
+        assertEq(l2VotingPower.name(), "Lisk Voting Power");
+        assertEq(l2VotingPower.symbol(), "vpLSK");
+    }
+
+    function setUp() public {
+        utils = new Utils();
+
+        // set initial values
+        lockingPositionContractAddress = address(0xdeadbeefdeadbeefdeadbeef);
+
+        console.log("L2VotingPowerTest address is: %s", address(this));
+
+        // deploy L2VotingPower Implementation contract
+        l2VotingPowerImplementation = new L2VotingPower();
+
+        // deploy L2VotingPower contract via proxy and initialize it at the same time
+        l2VotingPower = L2VotingPower(
+            address(
+                new ERC1967Proxy(
+                    address(l2VotingPowerImplementation),
+                    abi.encodeWithSelector(l2VotingPower.initialize.selector, lockingPositionContractAddress)
+                )
+            )
+        );
+
+        assertInitParamsEq();
+        assertEq(l2VotingPower.version(), "1.0.0");
+
+        // Upgrade from L2VotingPower to L2VotingPowerPaused, and call initializePaused
+        L2VotingPowerPaused l2VotingPowerPausedImplementation = new L2VotingPowerPaused();
+        l2VotingPower.upgradeToAndCall(
+            address(l2VotingPowerPausedImplementation),
+            abi.encodeWithSelector(l2VotingPowerPausedImplementation.initializePaused.selector)
+        );
+
+        // l2VotingPower pointing to paused contract
+        assertEq(l2VotingPower.version(), "1.0.0-paused");
+
+        // Ensure all other params are unchanged after paused contract update
+        assertInitParamsEq();
+    }
+
+    function test_AdjustVotingPower_NotPaused() public {
+        IL2LockingPosition.LockingPosition memory positionBefore =
+            IL2LockingPosition.LockingPosition(address(this), 50, 0, 0);
+        IL2LockingPosition.LockingPosition memory positionAfter =
+            IL2LockingPosition.LockingPosition(address(this), 100, 0, 0);
+
+        // call it as LockingPosition contract
+        vm.prank(lockingPositionContractAddress);
+
+        // Throws the require check on the first line, proving this function is not paused
+        vm.expectRevert("L2VotingPower: owner address cannot be 0");
+        l2VotingPower.adjustVotingPower(address(0), positionBefore, positionAfter);
+    }
+
+    function test_Delegate_Paused() public {
+        vm.expectRevert(L2VotingPowerPaused.VotingPowerIsPaused.selector);
+        l2VotingPower.delegate(address(0));
+    }
+
+    function test_DelegateBySig_Paused() public {
+        vm.expectRevert(L2VotingPowerPaused.VotingPowerIsPaused.selector);
+        l2VotingPower.delegateBySig(address(0), 0, 0, 0, "", "");
+    }
+
+    function test_UpgradeToAndCall_CanUpgradeFromPausedContractToNewContract() public {
+        MockL2VotingPowerV2 mockL2VotingPowerV2Implementation = new MockL2VotingPowerV2();
+
+        uint256 testNumber = 123;
+
+        // upgrade contract, and also change some variables by reinitialize
+        l2VotingPower.upgradeToAndCall(
+            address(mockL2VotingPowerV2Implementation),
+            abi.encodeWithSelector(mockL2VotingPowerV2Implementation.initializeV2.selector, testNumber)
+        );
+
+        // new testNumber variable introduced
+        assertEq(MockL2VotingPowerV2(address(l2VotingPower)).testNumber(), testNumber);
+
+        // new version updated
+        assertEq(l2VotingPower.version(), "2.0.0");
+    }
+}
