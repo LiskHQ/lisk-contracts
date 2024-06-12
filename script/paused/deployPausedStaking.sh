9aee560f9cd411a4be58b82749ca2e956456f896
Matjaz Verbole <verbole@gmail.com>
2024-06-12 07:27:50 +0200

    Add deploy shell script for paused Staking process

---
+++ script/paused/deployPausedStaking.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+echo "Instructing the shell to exit immediately if any command returns a non-zero exit status..."
+set -e
+echo "Done."
+
+echo "Navigating to the root directory of the project..."
+cd ../
+echo "Done."
+
+echo "Setting environment variables..."
+source .env
+echo "Done."
+
+echo "Creating $NETWORK directory inside deployment directory..."
+if [ -z "$NETWORK" ]
+then
+      echo "NETWORK variable inside .env file is not set. Please set NETWORK environment variable."
+      exit 1
+else
+      if [ -d "deployment/$NETWORK" ]
+      then
+            echo "Directory deployment/$NETWORK already exists."
+      else
+            mkdir deployment/$NETWORK
+      fi
+fi
+echo "Done."
+
+echo "Deploying and if enabled verifying L2LockingPositionPaused smart contract..."
+if [ -z "$CONTRACT_VERIFIER" ]
+then
+      forge script --rpc-url="$L2_RPC_URL" --broadcast -vvvv script/contracts/L2/paused/L2LockingPositionPaused.s.sol:L2LockingPositionPausedScript
+else
+      if [ $CONTRACT_VERIFIER = "blockscout" ]
+      then
+            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier blockscout --verifier-url $L2_VERIFIER_URL -vvvv script/contracts/L2/paused/L2LockingPositionPaused.s.sol:L2LockingPositionPausedScript
+      fi
+      if [ $CONTRACT_VERIFIER = "etherscan" ]
+      then        
+            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier etherscan --etherscan-api-key="$L2_ETHERSCAN_API_KEY" -vvvv script/contracts/L2/paused/L2LockingPositionPaused.s.sol:L2LockingPositionPausedScript
+      fi
+fi
+echo "Done."
+
+echo "Deploying and if enabled verifying L2RewardPaused smart contract..."
+if [ -z "$CONTRACT_VERIFIER" ]
+then
+      forge script --rpc-url="$L2_RPC_URL" --broadcast -vvvv script/contracts/L2/paused/L2RewardPaused.s.sol:L2RewardPausedScript
+else
+      if [ $CONTRACT_VERIFIER = "blockscout" ]
+      then
+            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier blockscout --verifier-url $L2_VERIFIER_URL -vvvv script/contracts/L2/paused/L2RewardPaused.s.sol:L2RewardPausedScript
+      fi
+      if [ $CONTRACT_VERIFIER = "etherscan" ]
+      then        
+            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier etherscan --etherscan-api-key="$L2_ETHERSCAN_API_KEY" -vvvv script/contracts/L2/paused/L2RewardPaused.s.sol:L2RewardPausedScript
+      fi
+fi
+echo "Done."
