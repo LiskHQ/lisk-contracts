0137ae6a0f6cd182cd2da1844fcd5493508d3093
Alessandro Ricottone <ricott2@gmail.com>
2024-06-17 10:00:03 +0200

    fixing e2e

--- script/swap_and_bridge/example/e2e_test.sh
+++ script/swap_and_bridge/example/e2e_test.sh
@@ -13,11 +13,11 @@ source .env
 echo "Done."
 
 echo "Running E2E tests for Lido contract..."
-forge script -vv --rpc-url "http://localhost:8545" script/swap_and_bridge/contracts/SwapAndBridge_lido_e2e_test.s.sol:TestLidoBridgingL1Script
-forge script -vv --rpc-url "http://localhost:8546" script/swap_and_bridge/contracts/SwapAndBridge_lido_e2e_test.s.sol:TestLidoBridgingL2Script
+forge script -vvvv --rpc-url "http://localhost:8545" script/swap_and_bridge/contracts/SwapAndBridge_e2e_test.s.sol:TestE2EL1Script $L1_LIDO_BRIDGE_ADDR $L1_TOKEN_ADDR_LIDO $L2_LIDO_BRIDGE_ADDR $L2_TOKEN_ADDR_LIDO --sig 'run(address,address,address,address)'
+forge script -vvvv --rpc-url "http://localhost:8546" script/swap_and_bridge/contracts/SwapAndBridge_e2e_test.s.sol:TestE2EL2Script $L2_TOKEN_ADDR_LIDO --sig 'run(address)'
 echo "Done."
 
 echo "Running E2E tests for Diva contract..."
-forge script -vv --rpc-url "http://localhost:8545" script/swap_and_bridge/contracts/SwapAndBridge_diva_e2e_test.s.sol:TestDivaBridgingL1Script
-forge script -vv --rpc-url "http://localhost:8546" script/swap_and_bridge/contracts/SwapAndBridge_diva_e2e_test.s.sol:TestDivaBridgingL2Script
+forge script -vv --rpc-url "http://localhost:8545" script/swap_and_bridge/contracts/SwapAndBridge_e2e_test.s.sol:TestE2EL1Script $L1_STANDARD_BRIDGE_ADDR $L1_TOKEN_ADDR_DIVA $L2_STANDARD_BRIDGE_ADDR $L2_TOKEN_ADDR_DIVA --sig 'run(address,address,address,address)'
+forge script -vv --rpc-url "http://localhost:8546" script/swap_and_bridge/contracts/SwapAndBridge_e2e_test.s.sol:TestE2EL2Script $L2_TOKEN_ADDR_DIVA --sig 'run(address)'
 echo "Done."
