490c53d851f5c4e9694f921a5c7f4864f74c6ca6
Matjaz Verbole <verbole@gmail.com>
2024-06-20 09:27:56 +0200

    Remove deploying DAO when deploying Staking process

--- script/paused/deployPausedStaking.sh
+++ script/paused/deployPausedStaking.sh
@@ -31,46 +31,6 @@ echo "Cleaning up the build artifacts to be able to deploy the contract..."
 forge clean
 echo "Done."
 
-echo "Deploying and if enabled verifying L2GovernorPaused smart contract..."
-if [ -z "$CONTRACT_VERIFIER" ]
-then
-      forge script --rpc-url="$L2_RPC_URL" --broadcast -vvvv script/contracts/L2/paused/L2GovernorPaused.s.sol:L2GovernorPausedScript
-else
-      if [ $CONTRACT_VERIFIER = "blockscout" ]
-      then
-            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier blockscout --verifier-url $L2_VERIFIER_URL -vvvv script/contracts/L2/paused/L2GovernorPaused.s.sol:L2GovernorPausedScript
-      fi
-      if [ $CONTRACT_VERIFIER = "etherscan" ]
-      then
-            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier etherscan --etherscan-api-key="$L2_ETHERSCAN_API_KEY" -vvvv script/contracts/L2/paused/L2GovernorPaused.s.sol:L2GovernorPausedScript
-      fi
-fi
-echo "Done."
-
-echo "Cleaning up the build artifacts to be able to deploy the next contract..."
-forge clean
-echo "Done."
-
-echo "Deploying and if enabled verifying L2VotingPowerPaused smart contract..."
-if [ -z "$CONTRACT_VERIFIER" ]
-then
-      forge script --rpc-url="$L2_RPC_URL" --broadcast -vvvv script/contracts/L2/paused/L2VotingPowerPaused.s.sol:L2VotingPowerPausedScript
-else
-      if [ $CONTRACT_VERIFIER = "blockscout" ]
-      then
-            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier blockscout --verifier-url $L2_VERIFIER_URL -vvvv script/contracts/L2/paused/L2VotingPowerPaused.s.sol:L2VotingPowerPausedScript
-      fi
-      if [ $CONTRACT_VERIFIER = "etherscan" ]
-      then
-            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier etherscan --etherscan-api-key="$L2_ETHERSCAN_API_KEY" -vvvv script/contracts/L2/paused/L2VotingPowerPaused.s.sol:L2VotingPowerPausedScript
-      fi
-fi
-echo "Done."
-
-echo "Cleaning up the build artifacts to be able to deploy the next contract..."
-forge clean
-echo "Done."
-
 echo "Deploying and if enabled verifying L2LockingPositionPaused smart contract..."
 if [ -z "$CONTRACT_VERIFIER" ]
 then
