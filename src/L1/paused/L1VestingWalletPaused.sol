672409fe5f3f89b2d7d791a032026a79199cf673
Franco NG <franco.ng@lightcurve.io>
2024-07-09 18:00:09 +0200

    Add NatSpec comments

--- src/L1/paused/L1VestingWalletPaused.sol
+++ src/L1/paused/L1VestingWalletPaused.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.23;
 
-import "src/L2/paused/L2VestingWalletPaused.sol";
+import { L2VestingWalletPaused } from "src/L2/paused/L2VestingWalletPaused.sol";
 
+/// @title L1VestingWalletPaused - Paused version of L1VestingWallet contract
+/// @notice This contract is used to pause the L1VestingWallet contract. In case of any emergency, the owner can upgrade
+/// and
+///         pause the contract to prevent any further vesting operations.
+///         L1VestingWalletPaused shares the same functionality of L1VestingWalletPaused.
 contract L1VestingWalletPaused is L2VestingWalletPaused { }
