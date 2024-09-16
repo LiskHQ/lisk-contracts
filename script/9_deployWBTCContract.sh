b77937662a593cc247487106945e3d282a90ec71
ricott1 <ricott2@gmail.com>
2024-09-16 10:26:31 +0200

    feat: add WBTC deployment script and addresses for mainnet and sepolia
    Signed-off-by: ricott1 <ricott2@gmail.com>

--- script/9_deployWBTCContract.sh
+++ script/9_deployWBTCContract.sh
@@ -12,5 +12,9 @@ echo "Setting environment variables..."
 source .env
 echo "Done."
 
+<<<<<<< HEAD
 echo "Deploying OptimismMintableERC20 WBTC token smart contract..."
+=======
+echo "Deploying OptimismMintableERC20 USDT token smart contract..."
+>>>>>>> 8914457 (feat: add WBTC deployment script and addresses for mainnet and sepolia)
 cast send --rpc-url $L2_RPC_URL "0x4200000000000000000000000000000000000012" "createOptimismMintableERC20WithDecimals(address,string,string,uint8)" $REMOTE_TOKEN_ADDR_WBTC "Wrapped BTC" "WBTC" 8 -i
