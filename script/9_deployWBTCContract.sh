2caaf0abb82c9fe94ef0e6e182e8bb7bc23dd5c9
ricott1 <ricott2@gmail.com>
2024-09-17 09:14:26 +0200

    Fix log typo
    Signed-off-by: ricott1 <ricott2@gmail.com>

--- script/9_deployWBTCContract.sh
+++ script/9_deployWBTCContract.sh
@@ -12,5 +12,5 @@ echo "Setting environment variables..."
 source .env
 echo "Done."
 
-echo "Deploying OptimismMintableERC20 USDT token smart contract..."
+echo "Deploying OptimismMintableERC20 WBTC token smart contract..."
 cast send --rpc-url $L2_RPC_URL "0x4200000000000000000000000000000000000012" "createOptimismMintableERC20WithDecimals(address,string,string,uint8)" $REMOTE_TOKEN_ADDR_WBTC "Wrapped BTC" "WBTC" 8 -i
