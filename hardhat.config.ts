f1482ef065bb713bb46e448695c3a0ae92de2dc8
Incede <33103370+Incede@users.noreply.github.com>
2024-10-23 16:10:31 +0200

    Init script and env

---
+++ hardhat.config.ts
@@ -0,0 +1,94 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+// PLUGINS
+import "@gelatonetwork/web3-functions-sdk/hardhat-plugin";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+
+// Process Env Variables
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
+
+const PK = process.env.PK;
+const ALCHEMY_ID = process.env.ALCHEMY_ID;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+// HardhatUserConfig bug
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const config: HardhatUserConfig = {
+  // web3 functions
+  w3f: {
+    rootDir: "./web3-functions",
+    debug: false,
+    networks: ["hardhat", "mumbai"], //(multiChainProvider) injects provider for these networks
+  },
+  // hardhat-deploy
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  defaultNetwork: "liskSepolia",
+
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_ID}`,
+        blockNumber: 35241432,
+      },
+    },
+
+    ethereum: {
+      accounts: PK ? [PK] : [],
+      chainId: 1,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_ID}`,
+    },
+    mumbai: {
+      accounts: PK ? [PK] : [],
+      chainId: 80001,
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_ID}`,
+    },
+    polygon: {
+      accounts: PK ? [PK] : [],
+      chainId: 137,
+      url: "https://polygon-rpc.com",
+    },
+    liskSepolia: {
+      accounts: PK ? [PK] : [],
+      chainId: 4202,
+      url: "https://rpc.sepolia-api.lisk.com",
+    },
+    lisk: {
+      accounts: PK ? [PK] : [],
+      chainId: 1135,
+      url: "https://rpc.api.lisk.com",
+    },
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+        },
+      },
+    ],
+  },
+
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+
+  // hardhat-deploy
+  verify: {
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY ? ETHERSCAN_API_KEY : "",
+    },
+  },
+};
+
+export default config;
