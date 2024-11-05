22a63ec71724e2f66871d0ba311204c4291dd974
Matjaz Verbole <verbole@gmail.com>
2024-11-05 16:05:30 +0100

    Added ETH/USD token pair

--- script/11_deployPriceFeedsWithoutRounds.sh
+++ script/11_deployPriceFeedsWithoutRounds.sh
@@ -74,3 +74,19 @@ else
       fi
 fi
 echo "Done."
+
+echo "Deploying and if enabled verifying L2PriceFeedWithoutRounds smart contract for ETH/USD..."
+if [ -z "$CONTRACT_VERIFIER" ]
+then
+      forge script --rpc-url="$L2_RPC_URL" --broadcast -vvvv --sig "run(string,string)" script/contracts/L2/L2PriceFeedWithoutRounds.s.sol:L2PriceFeedWithoutRoundsScript "ETH" "PrimaryProd"
+else
+      if [ $CONTRACT_VERIFIER = "blockscout" ]
+      then
+            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier blockscout --verifier-url $L2_VERIFIER_URL -vvvv --sig "run(string,string)" script/contracts/L2/L2PriceFeedWithoutRounds.s.sol:L2PriceFeedWithoutRoundsScript "ETH" "PrimaryProd"
+      fi
+      if [ $CONTRACT_VERIFIER = "etherscan" ]
+      then        
+            forge script --rpc-url="$L2_RPC_URL" --broadcast --verify --verifier etherscan --etherscan-api-key="$L2_ETHERSCAN_API_KEY" -vvvv --sig "run(string,string)" script/contracts/L2/L2PriceFeedWithoutRounds.s.sol:L2PriceFeedWithoutRoundsScript "ETH" "PrimaryProd"
+      fi
+fi
+echo "Done."
