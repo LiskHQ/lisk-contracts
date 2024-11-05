427916e66df82ca27d756c0c2fd983ff737e944c
Incede <33103370+Incede@users.noreply.github.com>
2024-11-05 01:37:42 +0100

    Update w3f to support new oracle contract

--- web3-functions/index.ts
+++ web3-functions/index.ts
@@ -3,14 +3,14 @@ import {
   Web3FunctionContext,
 } from "@gelatonetwork/web3-functions-sdk";
 import { BigNumber, Contract } from "ethers";
+import { formatBytes32String } from "ethers/lib/utils";
 import { WrapperBuilder } from "@redstone-finance/evm-connector";
 
 const ORACLE_ABI = [
-  "function getStoredPrice() public view returns (uint256)",
-  "function getLivePrice() public view returns (uint256)",
-  "function getPriceDeviation() external view returns (uint)",
-  "function decimals() external pure returns (uint8)",
-  "function updatePrice() public",
+  "function updateDataFeedsValuesPartial(bytes32[]) public",
+  "function getValueForDataFeed(bytes32) public view returns (uint256)",
+  "function getDataTimestampFromLatestUpdate(bytes32) external view returns (uint256)",
+  "function getLivePrice(bytes32[]) public view returns (uint256[], uint256)",
 ];
 
 Web3Function.onRun(async (context: Web3FunctionContext) => {
@@ -18,45 +18,79 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
 
   const provider = multiChainProvider.default();
 
-  const oracleAddress = userArgs.oracleAddress as string;
-  const oracle = new Contract(oracleAddress, ORACLE_ABI, provider);
+  const oracleAddressPrimaryProd = "0x1038999DCf0A302Cc8Eed72fAeCbf0eEBfC476b0";
+  const oraclePrimaryProd = new Contract(oracleAddressPrimaryProd, ORACLE_ABI, provider);
+
+  const dataFeedIdEth = formatBytes32String("ETH");
+  const dataFeedIdUsdt = formatBytes32String("USDT");
 
   // Wrap contract with redstone data service
-  const wrappedOracle = WrapperBuilder.wrap(oracle).usingDataService(
+  const wrappedOraclePrimaryProd = WrapperBuilder.wrap(oraclePrimaryProd).usingDataService(
     {
-      dataServiceId: "redstone-rapid-demo",
-      uniqueSignersCount: 1,
-      dataFeeds: ["LSK"],
+      dataServiceId: "redstone-primary-prod",
+      uniqueSignersCount: 2,
+      dataFeeds: ["ETH", "USDT"],
       disablePayloadsDryRun: true,
     },
-    ["https://d33trozg86ya9x.cloudfront.net"]
+    ["https://oracle-gateway-1.a.redstone.finance"]
   );
 
   // Retrieve stored & live prices
-  const decimals = await wrappedOracle.decimals();
-  const livePrice: BigNumber = await wrappedOracle.getLivePrice();
-  const storedPrice: BigNumber = await wrappedOracle.getStoredPrice();
-  console.log(`Live price: ${livePrice.toString()}`);
-  console.log(`Stored price: ${storedPrice.toString()}`);
+  const decimals = BigNumber.from(8);
+  const { livePrices, liveTimestamp } = await wrappedOraclePrimaryProd.getLivePrice([dataFeedIdEth, dataFeedIdUsdt]);
+  const liveEthPrice: BigNumber = livePrices[0];
+  const liveUsdtPrice: BigNumber = livePrices[1];
+  const storedEthPrice: BigNumber = await wrappedOraclePrimaryProd.getValueForDataFeed(dataFeedIdEth);
+  const storedUsdtPrice: BigNumber = await wrappedOraclePrimaryProd.getValueForDataFeed(dataFeedIdUsdt);
+  console.log(`Live ETH price: ${liveEthPrice.toString()}`);
+  console.log(`Live USDT price: ${liveUsdtPrice.toString()}`);
+  console.log(`Stored ETH price: ${storedEthPrice.toString()}`);
+  console.log(`Stored USDT price: ${storedUsdtPrice.toString()}`);
 
   // Check price deviation
-  const deviation: BigNumber = await wrappedOracle.getPriceDeviation();
-  const deviationPrct = (deviation.toNumber() / 10 ** decimals) * 100;
+  const priceDeviationEth = computePriceDeviation(liveEthPrice, storedEthPrice, decimals);
+  const deviationPrct = (priceDeviationEth.toNumber() / 10 ** 8) * 100;
   console.log(`Deviation: ${deviationPrct.toFixed(2)}%`);
 
-  // Only update price if deviation is above 0.5%
+  // Check update time interval
+  const currentTimestamp = Date.now();
+  const timeElapsed = (currentTimestamp - liveTimestamp) / (1000 * 60 * 60)
+
+  // Only update price if deviation is above 0.5% or last update is more than 6 hours ago
   const minDeviation = 0.5;
-  if (deviationPrct < minDeviation) {
+  if (deviationPrct < minDeviation && timeElapsed <= 6) {
     return {
       canExec: false,
-      message: `No update: price deviation too small`,
+      message: `No update: price deviation too small or time elapsed since last update is less than 6 hours`,
     };
   }
 
   // Craft transaction to update the price on-chain
-  const { data } = await wrappedOracle.populateTransaction.updatePrice();
+  console.log(`Start price update for ETH`);
+  const { data } = await wrappedOraclePrimaryProd.populateTransaction.updateDataFeedsValuesPartial(
+    [dataFeedIdEth]
+  );
+  console.log(`Stop price update`);
+  console.log(`Data: ${data}`);
+
   return {
     canExec: true,
-    callData: [{ to: oracleAddress, data: data as string }],
+    callData: [{ to: oracleAddressPrimaryProd, data: data as string }],
   };
 });
+
+function computePriceDeviation(
+  newPrice: BigNumber,
+  oldPrice: BigNumber,
+  decimals: BigNumber
+) {
+  const zero = BigNumber.from(0);
+  const ten = BigNumber.from(10);
+  if (zero.eq(oldPrice)) {
+    return ten.mul(decimals);
+  } else if (newPrice.gt(oldPrice)) {
+    return ((newPrice.sub(oldPrice)).mul(ten).mul(decimals)).div(oldPrice);
+  } else {
+    return ((oldPrice.sub(newPrice)).mul(ten).mul(decimals)).div(oldPrice);
+  }
+}
