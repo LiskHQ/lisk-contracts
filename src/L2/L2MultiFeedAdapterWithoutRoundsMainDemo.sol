8f2a81fe65e6dd52d61893872deb83007e99f647
Matjaz Verbole <verbole@gmail.com>
2024-11-07 16:24:56 +0100

    Rename function to more expressional name

--- src/L2/L2MultiFeedAdapterWithoutRoundsMainDemo.sol
+++ src/L2/L2MultiFeedAdapterWithoutRoundsMainDemo.sol
@@ -70,7 +70,12 @@ contract L2MultiFeedAdapterWithoutRoundsMainDemo is
     /// @param dataFeedIds An array of unique data feed identifiers.
     /// @return An array of the extracted and verified oracle values in the same order as they are requested in the
     ///         dataFeedIds array and data packages timestamp.
-    function getLivePrice(bytes32[] memory dataFeedIds) public view virtual returns (uint256[] memory, uint256) {
+    function getLivePricesAndTimestamp(bytes32[] memory dataFeedIds)
+        public
+        view
+        virtual
+        returns (uint256[] memory, uint256)
+    {
         return getOracleNumericValuesAndTimestampFromTxMsg(dataFeedIds);
     }
 }
