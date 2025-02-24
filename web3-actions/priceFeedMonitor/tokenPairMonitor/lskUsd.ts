d47b95ed776769fa64a734716a38edf542f82d70
Sameer <sameer.subudhi@lisk.com>
2025-02-24 12:37:23 +0100

    Update web3 actions to use private RPC endpoint (#225)
    * switch package manager to yarn
    
    * setup prettier
    
    * format code with prettier
    
    * add support for custom rpc endpoint using tenderly secrets
    
    * switch back package manager to npm

--- web3-actions/priceFeedMonitor/tokenPairMonitor/lskUsd.ts
+++ web3-actions/priceFeedMonitor/tokenPairMonitor/lskUsd.ts
@@ -1,10 +1,5 @@
-import {
-	ActionFn,
-	Context,
-	Event,
-	PeriodicEvent,
-} from '@tenderly/actions';
-import { checkTokenPairPriceUpdateTime } from './validate';
+import { ActionFn, Context, Event, PeriodicEvent } from "@tenderly/actions";
+import { checkTokenPairPriceUpdateTime } from "./validate";
 
 // Define the contract address and token pair
 const CONTRACT_ADDRESS = "0xa1EbA9E63ed7BA328fE0778cFD67699F05378a96";
@@ -14,11 +9,18 @@ export const monitorLskUsdFn: ActionFn = async (context: Context, event: Event) 
 	const periodicEvent = event as PeriodicEvent;
 	console.log(periodicEvent);
 
-	// Get Opsgenie API key from the secrets
-	const apiKey = await context.secrets.get("OPSGENIE_API_KEY");
+	// Retrieve neecessary secrets from the context
+	const rpcHttpEndpoint = await context.secrets.get("LISK_RPC_HTTP_ENDPOINT");
+	const opsgenieApiKey = await context.secrets.get("OPSGENIE_API_KEY");
 
 	// Current time in seconds
 	const currentTimestamp = Math.floor(periodicEvent.time.getTime() / 1000);
 
-	await checkTokenPairPriceUpdateTime(CONTRACT_ADDRESS, TOKEN_PAIR, apiKey, currentTimestamp);
-}
+	await checkTokenPairPriceUpdateTime(
+		CONTRACT_ADDRESS,
+		TOKEN_PAIR,
+		currentTimestamp,
+		rpcHttpEndpoint,
+		opsgenieApiKey,
+	);
+};
--- web3-actions/priceFeedMonitor/tokenPairMonitor/wbtcUsd.ts
+++ web3-actions/priceFeedMonitor/tokenPairMonitor/wbtcUsd.ts
@@ -1,10 +1,5 @@
-import {
-	ActionFn,
-	Context,
-	Event,
-	PeriodicEvent,
-} from '@tenderly/actions';
-import { checkTokenPairPriceUpdateTime } from './validate';
+import { ActionFn, Context, Event, PeriodicEvent } from "@tenderly/actions";
+import { checkTokenPairPriceUpdateTime } from "./validate";
 
 // Define the contract address and token pair
 const CONTRACT_ADDRESS = "0x13da43eA89fB692bdB6666F053FeE70aC61A53cd";
@@ -14,11 +9,18 @@ export const monitorWbtcUsdFn: ActionFn = async (context: Context, event: Event)
 	const periodicEvent = event as PeriodicEvent;
 	console.log(periodicEvent);
 
-	// Get Opsgenie API key from the secrets
-	const apiKey = await context.secrets.get("OPSGENIE_API_KEY");
+	// Retrieve neecessary secrets from the context
+	const rpcHttpEndpoint = await context.secrets.get("LISK_RPC_HTTP_ENDPOINT");
+	const opsgenieApiKey = await context.secrets.get("OPSGENIE_API_KEY");
 
 	// Current time in seconds
 	const currentTimestamp = Math.floor(periodicEvent.time.getTime() / 1000);
 
-	await checkTokenPairPriceUpdateTime(CONTRACT_ADDRESS, TOKEN_PAIR, apiKey, currentTimestamp);
-}
+	await checkTokenPairPriceUpdateTime(
+		CONTRACT_ADDRESS,
+		TOKEN_PAIR,
+		currentTimestamp,
+		rpcHttpEndpoint,
+		opsgenieApiKey,
+	);
+};
